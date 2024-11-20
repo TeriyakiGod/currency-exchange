bd19c3609b492066a89614325c091fc69b264403
Kacper Akdag-Ochnik <kacperochnik01@gmail.com>
2024-11-20 19:11:46 +0100

    Defined currency and exchange rate models

--- currency_exchange/api/models.py
+++ currency_exchange/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Currency(models.Model):
+    code = models.CharField(max_length=3, unique=True)
+    name = models.CharField(max_length=50)
+
+class ExchangeRate(models.Model):
+    base_currency = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name='base_currency')
+    target_currency = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name='target_currency')
+    rate = models.DecimalField(max_digits=10, decimal_places=6)
+    date = models.DateField()
+
+    def __str__(self):
+        return f'{self.base_currency.code}{self.target_currency.code}'
